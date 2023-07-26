ca5865bc65fe54a426b96277fa7f1273f86c57e2
hendrix <leandrotully@gmail.com>
2023-07-26 07:16:29 -0300

    Dump and Not Exsits

--- SQL/10-MANY/EXISTS NOT.sql
+++ SQL/10-MANY/EXISTS NOT.sql
@@ -1,5 +1,29 @@
-use sql2;
-
-SELECT * FROM aluno  
-   WHERE NOT EXISTS ( SELECT * FROM matricula  
-					   WHERE matricula.aluno_id = aluno.id );
+use estudos;
+DROP TABLE if exists people;
+DROP TABLE if exists employee;
+CREATE TABLE people(
+   id int auto_increment primary key,
+   name char(10)
+);
+CREATE TABLE employee(
+   id int auto_increment primary key,
+   name char(10)
+);
+insert into people(name)
+values('leandro');
+insert into people(name)
+values('soares');
+insert into people(name)
+values('ribeiro');
+insert into employee(name)
+values('ribeiro');
+-- Busca apenas os registros que constam em People, 
+-- Mas não estão em Employee
+SELECT name
+FROM people p
+WHERE NAME = 'leandro'
+   AND NOT EXISTS (
+      select *
+      from employee e
+      where e.name = p.name
+   )
--- SQL/10-MANY/EXISTS.sql
+++
@@ -1,5 +0,0 @@
-use sql2;
-
-SELECT * from aluno A 
-   where exists ( select * from matricula M
-                   where M.aluno_id = A.id )
